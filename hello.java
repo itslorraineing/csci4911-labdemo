8f93e94ac59c35cf510f8659566da63be99f3e33
lorraine <ljimene2@my.hpu.edu>
2019-01-20 19:13:54 -1000

    added some, changed some

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
 public class hello {
     public static void main(String[] args) {
-        greeting("Hello!");
+        greeting("Hello?");
+        greeting("Is anybody there?");
     }
 
     public static void greeting(String msg) {
---
+++ riddle.java
@@ -0,0 +1,6 @@
+public class riddle {
+    public static void main(String[] args) {
+        System.out.println("what goes all across the country but never moves?");
+        System.out.println("a road");
+    }
+}
