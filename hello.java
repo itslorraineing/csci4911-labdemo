2ebd8d6548b6cc3364bf81eac2fd8635291cbae0
lorraine <ljimene2@my.hpu.edu>
2019-01-20 18:42:13 -1000

    added method

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        greeting("Hello!");
+    }
+
+    public static void greeting(String msg) {
+        System.out.println(msg);
     }
 }
