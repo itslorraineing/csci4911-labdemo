67e7c76b3f88dae0ef8071099a980d8cb4e37f3a
lorraine <ljimene2@my.hpu.edu>
2019-01-20 19:05:31 -1000

    added some, changed some

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
 public class hello {
     public static void main(String[] args) {
-        greeting("Hello!");
+        greeting("you say goodbye,");
+        greeting("and i say hello");
     }
 
     public static void greeting(String msg) {
